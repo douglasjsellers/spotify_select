6ec0289f3afdd4bc725096616198cc437cef0ce5
Douglas Sellers <doug@adhoclabs.co>
2018-09-15 22:20:59 -0700

    Now the array get's cleaned up a bit

--- get_selection.js
+++ get_selection.js
@@ -1,6 +1,7 @@
-var selection = document.getSelection();
+var selection = document.getSelection().toString().split( '\n' );
+var filteredSelection = selection.filter( function( value ) { return value != null && value.length > 0 } )
 var editorExtensionId = "pmegncfkljalkbbncambjfbmdjifneba";
-chrome.runtime.sendMessage(editorExtensionId, {selection: document.getSelection().toString().split( '\n' )},
+chrome.runtime.sendMessage(editorExtensionId, {selection: filteredSelection},
                            function(response) {
                              console.log( response );
                            });
