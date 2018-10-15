a9ecbdf64d10532c6f64c81ac8c7fd753173f305
Douglas Sellers <doug@adhoclabs.co>
2018-10-14 21:28:02 -0700

    Made a production version

--- get_selection.js
+++ get_selection.js
@@ -1,3 +1,3 @@
 var selection = document.getSelection().toString().split( '\n' );
 var filteredSelection = selection.filter( function( value ) { return value != null && value.length > 0 } )
-chrome.runtime.sendMessage("pmegncfkljalkbbncambjfbmdjifneba", {selection: filteredSelection},function(response) {});
+chrome.runtime.sendMessage("jekgbendlapagldnafgbcffcgjlpijmh", {selection: filteredSelection},function(response) {});
